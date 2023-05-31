e394361970e5207897bbce930ab1161551ab8fd0
BBaahaa <124143993+BBaahaa@users.noreply.github.com>
2023-05-31 19:08:56 +0100

    Add files via upload

---
+++ src/Components/Profile/Adress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Adress() {
+  return (
+    <div className='adress'>
+    <p className='adress'>Sayda / Monastir</p>  
+    </div>
+  )
+}
+
+export default Adress
---
+++ src/Components/Profile/FullName.js
@@ -0,0 +1,11 @@
+
+
+function FullName() {
+  return (
+    <div className="name">
+      <h3><i>Thamer Lafi</i></h3>
+    </div>
+  )
+}
+
+export default FullName
